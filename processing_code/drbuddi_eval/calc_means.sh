42a9bca55ba6bafa48ed2fda75e93fef7d56d9d1
Matt Cieslak <mattcieslak@gmail.com>
2025-09-13 22:40:07 -0400

    calculate drbuddi fa diffs

--- processing_code/drbuddi_eval/calc_means.sh
+++ processing_code/drbuddi_eval/calc_means.sh
@@ -4,17 +4,15 @@
 REPRO_DIR="${HOME}/rep1"
 
 cd ${REPRO_DIR}/separate_fa/appa_diffs_mni
+mkdir means
 
 for method in drbuddi nodrbuddi
-do
-for metric in FA AD
 do
     for sesid in ses-V02 ses-V03
     do
         3dmerge -gmean \
-            -1fmask means/nlin6_1.7mm_mask.nii.gz \
-            -prefix means/${method}_${sesid}_masked_${metric}.nii \
-            ${method}_sub-*_${sesid}*APPAdiff*${metric}.nii
+            -1fmask ${REPRO_DIR}/hbcd_pipeline_replication_guide/templates/nlin6_1.7mm_mask.nii.gz \
+            -prefix means/${method}_${sesid}_masked_FA.nii \
+            ${method}_sub-*_${sesid}*APPAdiff*FA.nii
     done
-done
 done
