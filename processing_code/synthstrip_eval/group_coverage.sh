2b68ea7cad4538cb56fd425582a005ecc397110e
mattcieslak <mattcieslak@gmail.com>
2025-09-14 11:29:29 -0400

    group_coverage

--- processing_code/synthstrip_eval/group_coverage.sh
+++ processing_code/synthstrip_eval/group_coverage.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-python threshold_masks.py ses-V02_nothresh_coverage.nii.gz ses-V02 --dice-threshold 1.0
+python threshold_masks.py ses-V02_coverage.nii.gz ses-V02 --dice-threshold 1.0
+python threshold_masks.py ses-V03_coverage.nii.gz ses-V03 --dice-threshold 1.0
+
 python threshold_masks.py ../ses-V02_thr0.1_coverage.nii.gz ses-V02 --dice-threshold 0.1
+python threshold_masks.py ../ses-V02_thr0.06_coverage.nii.gz ses-V02 --dice-threshold 0.06
 python threshold_masks.py ../ses-V02_thr0.12_coverage.nii.gz ses-V02 --dice-threshold 0.12
